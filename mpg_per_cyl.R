ec1f249915b8a1a71b6d419c0c41a5e8eceb2837
Christian Zang <christian.zang@hswt.de>
2023-02-16 11:30:51 +0100

    added a plot
    as usual, I am ignoring the products and only track the source files.

--- mpg_per_cyl.R
+++ mpg_per_cyl.R
@@ -1,5 +1,12 @@
 data(mtcars)
 library(dplyr)
+library(ggplot2)
+
 mpg_by_cyl <- mtcars %>% 
   group_by(cyl) %>% 
   summarise(avg_mpg = mean(mpg))
+
+ggplot(mtcars) +
+  geom_point(aes(cyl, mpg))
+
+ggsave("cyl_vs_mpg.pdf")
