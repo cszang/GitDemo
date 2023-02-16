d2f58f0a9ff14841662d8681bf1b20cb843a6041
Christian Zang <christian.zang@hswt.de>
2023-02-16 11:32:37 +0100

    added experimental smoothing feature

--- mpg_per_cyl.R
+++ mpg_per_cyl.R
@@ -6,7 +6,8 @@ mpg_by_cyl <- mtcars %>%
   group_by(cyl) %>% 
   summarise(avg_mpg = mean(mpg))
 
-ggplot(mtcars) +
-  geom_point(aes(cyl, mpg))
+ggplot(mtcars, aes(cyl, mpg)) +
+  geom_point() +
+  geom_smooth()
 
 ggsave("cyl_vs_mpg.pdf")
